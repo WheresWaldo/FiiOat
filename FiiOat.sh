3e36405abc90b7dcdb3e13cfd91b108765614d76
WheresWaldo <bud.hammerton@gmail.com>
2025-09-06 11:06:55 -0400

    Adjusting file lists
    Adding files to background and removing them from priority list.
    Increment version number.

--- FiiOat.sh
+++ FiiOat.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-# FIIOAT v17p
+# FIIOAT v17q
 # Author: @WheresWaldo (Github)
 # ×××××××××××××××××××××××××× #
 
@@ -87,7 +87,7 @@ ZRAM_PATH="/dev/zram0"
 
 
 # Log starting information
-log_info "Starting FiiOat v17p"
+log_info "Starting FiiOat v17q"
 log_info "Build Date: 09/04/2025"
 log_info "Author: @WheresWaldo (Github/Head-Fi)"
 log_info "Device: $(getprop ro.product.system.model)"
@@ -330,6 +330,7 @@ pm disable-user --user 0 "com.android.internal.systemui.navbar.twobutton"
 pm disable-user --user 0 "com.android.managedprovisioning"
 pm disable-user --user 0 "com.android.traceur"
 pm disable-user --user 0 "com.example.fiiotestappliction"
+pm disable-user --user 0 "com.google.android.apps.nbu.files"
 pm disable-user --user 0 "com.google.android.syncadapters.calendar"
 pm disable-user --user 0 "com.google.android.syncadapters.contacts"
 pm disable-user --user 0 "com.kk.xx.analyzer"
@@ -358,8 +359,10 @@ log_info "Done."
 # cmd appops set <package_name> RUN_ANY_IN_BACKGROUND ignore
 log_info "Setting run in background permisssions..."
 cmd appops set android RUN_ANY_IN_BACKGROUND ignore
+cmd appops set com.android.aboutfiio RUN_ANY_IN_BACKGROUND ignore
 cmd appops set android.ext.services RUN_ANY_IN_BACKGROUND ignore
 cmd appops set android.ext.shared RUN_ANY_IN_BACKGROUND ignore
+cmd appops set com.amazon.mp3 RUN_ANY_IN_BACKGROUND ignore
 cmd appops set com.android.certinstaller RUN_ANY_IN_BACKGROUND ignore
 cmd appops set com.android.chrome RUN_ANY_IN_BACKGROUND ignore
 cmd appops set com.android.connectivity.resources RUN_ANY_IN_BACKGROUND ignore
@@ -380,6 +383,12 @@ cmd appops set com.android.systemui RUN_ANY_IN_BACKGROUND ignore
 cmd appops set com.android.systemui RUN_ANY_IN_BACKGROUND ignore
 cmd appops set com.android.vending RUN_ANY_IN_BACKGROUND ignore
 cmd appops set com.android.wifi.resources RUN_ANY_IN_BACKGROUND ignore
+cmd appops set com.fiio.devicevendor RUN_ANY_IN_BACKGROUND ignore
+cmd appops set com.fiio.fiioeq RUN_ANY_IN_BACKGROUND ignore
+cmd appops set com.fiio.entersleep RUN_ANY_IN_BACKGROUND ignore
+cmd appops set com.fiio.market RUN_ANY_IN_BACKGROUND ignore
+cmd appops set com.fiio.scrcpy RUN_ANY_IN_BACKGROUND ignore
+cmd appops set com.fiio.tape RUN_ANY_IN_BACKGROUND ignore
 cmd appops set com.google.android.ext.shared RUN_ANY_IN_BACKGROUND ignore
 cmd appops set com.google.android.gms RUN_ANY_IN_BACKGROUND ignore
 cmd appops set com.google.android.gsf RUN_ANY_IN_BACKGROUND ignore
@@ -404,10 +413,7 @@ dumpsys deviceidle whitelist +com.bandcamp.android
 dumpsys deviceidle whitelist +com.cca.app_noble
 dumpsys deviceidle whitelist +com.extreamsd.usbaudioplayerpro
 dumpsys deviceidle whitelist +com.fiio.android
-dumpsys deviceidle whitelist +com.fiio.devicevendor
-dumpsys deviceidle whitelist +com.fiio.entersleep
 dumpsys deviceidle whitelist +com.fiio.fiioeq
-dumpsys deviceidle whitelist +com.fiio.market
 dumpsys deviceidle whitelist +com.fiio.music
 dumpsys deviceidle whitelist +com.fiio.scrcpy
 dumpsys deviceidle whitelist +com.fiio.tape
